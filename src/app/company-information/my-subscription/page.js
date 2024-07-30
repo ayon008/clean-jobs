1a6bc64d083664bb83e03e3f6b5cc8cc43addb41
ayon008 <shariar.ayon128@gmail.com>
2024-07-31 01:18:09 +0600

    email-credential-added

--- src/app/company-information/my-subscription/page.js
+++ src/app/company-information/my-subscription/page.js
@@ -4,7 +4,9 @@ import SectionTitles from "@/Components/SectionTitles";
 const page = () => {
     return (
         <div className="px-10 py-40 bg-[#DDD]">
-            {/* <SectionTitles heading={"Start your free 7 days"} subHeading={"No commitments. Cancel anytime"}/> */}
+            <SectionTitles heading={"Start your free 7 days"} subHeading={"No commitments. Cancel anytime"}/>
+
+            
             <SectionTitles heading={"Find the right plan"} subHeading={"Invest in your company's future with our comprehensive financial solution. Contact us for pricing details and see how we can help you streamline your finances and reach your business goals."} />
             <div>
                 <Pricing />
